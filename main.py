293041f89657fb615bb7634a9dcc05e184381d2d
DrinkWaterTwice <12110425@mail.sustech.edu.cn>
2026-02-04 23:32:27 +0800

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 from .tts.tts_api import TTSClient
 
-client = TTSClient()
+
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
@@ -22,6 +22,7 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        client = TTSClient()
         client.synthesize_and_play_realtime(message_str)  # 调用 TTSClient 将文本转换为语音并发送
 
     async def terminate(self):
