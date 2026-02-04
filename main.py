1d9d282a7dd2a6ba66b234a8a903dca69113ecc3
DrinkWaterTwice <12110425@mail.sustech.edu.cn>
2026-02-04 23:42:07 +0800

    1

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class MyPlugin(Star):
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-        self.client = TTSClient()
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("yuyin")
     async def yuyin(self, event: AstrMessageEvent):
@@ -21,8 +20,8 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        
-        await self.client.synthesize_and_play_realtime(message_str)  # 调用 TTSClient 将文本转换为语音并发送
+        client = TTSClient()
+        await client.synthesize_and_play_realtime(message_str)  # 调用 TTSClient 将文本转换为语音并发送
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
 
